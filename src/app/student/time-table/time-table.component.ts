b86d692ff5f85ddbe67d3c52c2bfa2f8711f5e1d
anurag <anuraganalogit@gmail.com>
2021-12-23 12:11:10 +0530

    studenttimetableissue

--- src/app/student/time-table/time-table.component.ts
+++ src/app/student/time-table/time-table.component.ts
@@ -12,7 +12,7 @@ export class TimeTableComponent implements OnInit {
   weekdays : any;timeslots : any;timetable : any;
   MainData: any;
   listOfInfo: any;
-
+  timetablearr = [];
   constructor(private _location:Location, private student:StudentService) { }
 
   ngOnInit(): void {
@@ -33,38 +33,40 @@ export class TimeTableComponent implements OnInit {
         var key = "test";
         testobj[key] = "";
         
-        console.log('testobj',testobj);
+        //console.log('testobj',testobj);
         for(var i=0 ; i<data_test.length; i++){
           var days = {};
           var day_key = data_test[i].weekDays;
           for(var j=0; j< day_key.length ; j++){  
             
             days[day_key[j]] = data_test[i].listOfInfo;
-            console.log('day_keys',days[day_key[j]]);
+            //console.log('day_keys',days[day_key[j]]);
             // var subjectandteacher = {};
             // var st_key = "subject_teacher";
             for(var a=0; a<days[day_key[j]].length; a+=2){
               var subjectandteacher = {};
               var st_key = "subject_teacher";
               newarr.push({subject:days[day_key[j]][a],teacher:days[day_key[j]][a+1]});
-              console.log('newarr',newarr);
+              //console.log('newarr',newarr);
               newarr = newarr.filter((value, index, self) =>
               index === self.findIndex((t) => (
                 t.subject === value.subject && t.teacher === value.teacher
               ))
 )
               subjectandteacher[st_key]= newarr;
-              
             }
+            days[day_key[j]]=subjectandteacher;
           }
           
           //days[day_key] = data_test[i].listOfInfo;
-          console.log('day_keys',days);
-          console.log('data_test',data_test[i]);
-          console.log('subjectandteacher',subjectandteacher);
-          console.log('data_days_week_days',data_test[i].weekDays);
-          console.log('data_test_listOfInfo',data_test[i].listOfInfo);
+          //console.log('daysobj',days);
+          // console.log('data_test',data_test[i]);
+          // console.log('subjectandteacher',subjectandteacher);
+          // console.log('data_days_week_days',data_test[i].weekDays);
+          // console.log('data_test_listOfInfo',data_test[i].listOfInfo);
         }
+        this.timetablearr.push({time:data.timeSlotsDTO,teacherandsuject:days});
+        console.log('timetable',this.timetablearr);
         return;
         this.timeslots = data.timeSlotsDTO;
         this.timetable = data.timeTableInfoDTO;
